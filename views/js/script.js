ebf731ecff0398c3e5cf8a46989056eb617f67bc
Manmeet Tarun <meetutarun16@gmail.com>
2017-07-11 23:22:51 +0530

    Frontend Done

--- views/js/script.js
+++ views/js/script.js
@@ -1,13 +1,13 @@
-var colors = ['#de302f','#f28001','#14f2a0'];
-
-$(function() {
-  var i=0;
-  setInterval(function () {
-    i =(i+1) % colors.length;
-    $('body').css({
-      'transition': '0.3s ease-in-out',
-      "background-color": colors[i]
-    });
-  }, 4000);
-
-});
+// var colors = ['#de302f','#f28001','#14f2a0'];
+//
+// $(function() {
+//   var i=0;
+//   setInterval(function () {
+//     i =(i+1) % colors.length;
+//     $('body').css({
+//       'transition': '0.3s ease-in-out',
+//       "background-color": colors[i]
+//     });
+//   }, 4000);
+//
+// });
